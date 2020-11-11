02a94dd71b4afcaecdfafbfeb2de3050f5512cfb
UnGerardo <gerardojdls@gmail.com>
2020-11-10 17:39:07 -0800

    Can set threshold and get current battery level

--- app/src/main/java/com/example/android/batteryhero/MainActivity.java
+++ app/src/main/java/com/example/android/batteryhero/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.android.batteryhero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editBatteryLevel;
+    TextView setLevel;
+    TextView currentLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editBatteryLevel = findViewById(R.id.et_battery_level);
+        setLevel = findViewById(R.id.tv_current_set_level);
+        currentLevel = findViewById(R.id.tv_current_level);
+    }
+
+    public void setBatteryLevel(View view) {
+        String level = editBatteryLevel.getText().toString();
+        setLevel.setText("Current Set Level: " + level + "%");
+    }
+
+    public void getCurrentBatteryLevel(View view) {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = registerReceiver(null, ifilter);
+
+        assert batteryStatus != null;
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level * 100 / (float)scale;
+        String currentLevelString = String.valueOf(batteryPct);
+
+        currentLevel.setText("Current Battery Level: " + currentLevelString + "%");
     }
 }
