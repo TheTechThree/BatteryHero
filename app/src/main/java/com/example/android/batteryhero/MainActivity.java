3e00d0116e88b6e4691737d728627fa4d2200f0f
UnGerardo <gerardojdls@gmail.com>
2020-11-20 16:04:49 -0800

    Started func to close background apps

--- app/src/main/java/com/example/android/batteryhero/MainActivity.java
+++ app/src/main/java/com/example/android/batteryhero/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.android.batteryhero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,5 +51,28 @@ public class MainActivity extends AppCompatActivity {
         String currentLevelString = String.valueOf(batteryPct);
 
         currentLevel.setText("Current Battery Level: " + currentLevelString + "%");
+
+        closeBackGroundApps(getApplicationContext());
+    }
+
+    public void closeBackGroundApps(Context c) {
+        List<ApplicationInfo> packages;
+        PackageManager pm;
+        pm = getPackageManager();
+        //get a list of installed apps. maybe get running apps instead
+        packages = pm.getInstalledApplications(0);
+
+        ActivityManager mActivityManager = (ActivityManager)c.getSystemService(Context.ACTIVITY_SERVICE);
+
+        for (ApplicationInfo packageInfo : packages) {
+            Log.d("Run", String.valueOf(packageInfo.flags)); // certain packageInfo.flags numbers for each name
+            if(!((packageInfo.flags) ==8388608)) { continue; } //maybe if 'google' 'example' and 'codepath' are in the name
+//            if(packageInfo.packageName.contains("android")) { continue; }
+            Log.d("Run", String.valueOf(packageInfo.flags & ApplicationInfo.FLAG_SYSTEM));
+            if(packageInfo.packageName.equals("com.example.android.batteryhero")) { continue; }
+//            Log.d("Run", "Got to point 3");
+            mActivityManager.killBackgroundProcesses(packageInfo.packageName);
+            Log.d("Run", "Killed " + packageInfo.packageName);
+        }
     }
 }
