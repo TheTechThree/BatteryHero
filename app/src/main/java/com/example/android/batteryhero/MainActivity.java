2a07a9b6ca115fea539f859284de62fd9f88d58d
UnGerardo <gerardojdls@gmail.com>
2020-12-02 16:17:54 -0800

    Close appropriate bg apps

--- app/src/main/java/com/example/android/batteryhero/MainActivity.java
+++ app/src/main/java/com/example/android/batteryhero/MainActivity.java
@@ -83,14 +83,103 @@ public class MainActivity extends AppCompatActivity {
         ActivityManager mActivityManager = (ActivityManager)c.getSystemService(Context.ACTIVITY_SERVICE);
 
         for (ApplicationInfo packageInfo : packages) {
-            Log.d("Run", String.valueOf(packageInfo.flags)); // certain packageInfo.flags numbers for each name
-            if(!((packageInfo.flags) ==8388608)) { continue; } //maybe if 'google' 'example' and 'codepath' are in the name
-//            if(packageInfo.packageName.contains("android")) { continue; }
-            Log.d("Run", String.valueOf(packageInfo.flags & ApplicationInfo.FLAG_SYSTEM));
-            if(packageInfo.packageName.equals("com.example.android.batteryhero")) { continue; }
-//            Log.d("Run", "Got to point 3");
-            mActivityManager.killBackgroundProcesses(packageInfo.packageName);
-            Log.d("Run", "Killed " + packageInfo.packageName);
+            switch (packageInfo.packageName) {
+                case "com.example.android.batteryhero":
+                case "com.android.cts.priv.ctsshim":
+                case "com.google.android.ext.services":
+                case "com.android.providers.telephony":
+                case "com.google.android.googlequicksearchbox":
+                case "com.android.providers.calendar":
+                case "com.android.providers.media":
+                case "com.google.android.onetimeinitializer":
+                case "com.google.android.ext.shared":
+                case "com.android.protips":
+                case "com.android.documentsui":
+                case "com.android.externalstorage":
+                case "com.android.htmlviewer":
+                case "com.android.companiondevicemanager":
+                case "com.android.mms.service":
+                case "com.android.providers.downloads":
+                case "com.google.android.apps.messaging":
+                case "com.google.android.configupdater":
+                case "com.android.defcontainer":
+                case "com.google.ar.core":
+                case "com.android.providers.downloads.ui":
+                case "com.android.vending":
+                case "com.android.pacprocessor":
+                case "com.android.certinstaller":
+                case "com.android.carrierconfig":
+                case "android":
+                case "com.android.contacts":
+                case "com.android.camera2":
+                case "com.google.android.apps.inputmethod.hindi":
+                case "com.android.egg":
+                case "com.android.mtp":
+                case "com.android.backupconfirm":
+                case "com.google.android.deskclock":
+                case "com.android.statementservice":
+                case "com.google.android.gm":
+                case "com.android.systemui.theme.dark":
+                case "com.google.android.setupwizard":
+                case "com.android.providers.settings":
+                case "com.android.sharedstoragebackup":
+                case "com.google.android.music":
+                case "com.android.printspooler":
+                case "com.android.dreams.basic":
+                case "com.android.inputdevices":
+                case "com.google.android.dialer":
+                case "com.android.bips":
+                case "com.android.cellbroadcastreceiver":
+                case "com.google.android.webview":
+                case "com.android.server.telecom":
+                case "com.google.android.syncadapters.contacts":
+                case "com.android.keychain":
+                case "com.google.android.packageinstaller":
+                case "com.google.android.gms":
+                case "com.google.android.gsf":
+                case "com.google.android.tts":
+                case "com.ustwo.lwp":
+                case "com.android.calllogbackup":
+                case "com.google.android.partnersetup":
+                case "com.google.android.apps.wallpaper.nexus":
+                case "com.google.android.apps.nexuslauncher":
+                case "com.android.proxyhandler":
+                case "com.breel.geswallpapers":
+                case "org.chromium.webview_shell":
+                case "com.google.android.feedback":
+                case "com.google.android.printservice.recommendation":
+                case "com.android.managedprovisioning":
+                case "com.android.providers.partnerbookmarks":
+                case "com.android.wallpaper.livepicker":
+                case "com.android.netspeed":
+                case "com.google.android.sdksetup":
+                case "com.google.android.backuptransport":
+                case "com.android.storagemanager":
+                case "jp.co.omronsoft.openwnn":
+                case "com.android.bookmarkprovider":
+                case "com.android.settings":
+                case "com.google.android.inputmethod.pinyin":
+                case "com.android.cts.ctsshim":
+                case "com.android.vpndialogs":
+                case "com.google.android.apps.wallpaper":
+                case "com.android.phone":
+                case "com.android.shell":
+                case "com.android.wallpaperbackup":
+                case "com.android.providers.blockednumber":
+                case "com.android.providers.userdictionary":
+                case "com.android.emergency":
+                case "com.android.location.fused":
+                case "com.android.systemui":
+                case "com.android.customlocale2":
+                case "com.android.development":
+                case "com.android.providers.contacts":
+                case "com.android.captiveportallogin":
+                case "com.google.android.inputmethod.latin":
+                    break;
+                default:
+                    mActivityManager.killBackgroundProcesses(packageInfo.packageName);
+                    Log.d("Runn", "Killed " + packageInfo.packageName);
+            }
         }
     }
 }
